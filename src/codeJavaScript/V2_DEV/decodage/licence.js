47b53e452ef5885fe4c07b8417434c7f54e6ffc1
Chabanat Matis <mchabanat@iutbayonne.univ-pau.fr>
2022-11-18 17:02:16 +0100

    Test d'une fonction d'une prise vidéo, pour optimisation de la capture du code

---
+++ src/codeJavaScript/V2_DEV/decodage/licence.js
@@ -0,0 +1,84 @@
+//Classe Licence 
+
+class Licence {
+    //Constructeur de l'objet Licence
+    constructor(l="") {
+      this.numLicence = l;
+    }
+
+    //Encapsulation
+    //Fonction qui retourne l'attribut numLicence
+    getNumLicence(){
+        return this.numLicence;
+    }
+    setNumLicence(l=""){
+        this.numLicence=l;
+    }
+
+    //Méthode Spécifique
+    //Méthode : Aucune données >> Méthode convertionGrayCode >> licenceGrayCode
+    convertionGrayCode(){
+        //licenceGrayCode >> Initialisation des variables >> licenceGrayCode
+        var licenceGrayCode=""
+        //Parcours complet du numéro de licence avec traitement systématique (conversion gray code)
+        for(var caracCourant = 0 ;caracCourant < this.numLicence.length ; caracCourant++){
+            //licenceGrayCode, TABLE_ENCODAGE >> Traduction du caractère courant en graycode >> licenceGrayCode
+            licenceGrayCode+=(TABLE_ENCODAGE[this.numLicence[caracCourant]]);
+        }
+        //Retourne la licence convertie en gray code
+        return licenceGrayCode;
+    }
+
+    //Méthode : licenceGrayCode >> conversionBarre >> liteBarre
+    conversionBarres(licenceGrayCode){
+        //listeBarre >> Initialisation de liste barres >> listeBarre
+        var listeBarre=[]
+        //Parcours complet de licenceGrayCode avec traitement systématique qui transforme 3 bits de la liste en  taille de barre
+        for(var i=0;i<licenceGrayCode.length;i+=3){
+            //Séparation de la liste gray code en motBinaire de 3 bits, Conversion motBinaire en taille barre
+            //motBinaire >> Initialisation du mot binaire >> motBinaire
+            var motBinaire=[];
+            //Affectation mot binaire
+            for(var decalage=0;decalage<3;decalage++){
+                //motBinaire, licenceGrayCode >> Ajout du bit courant dans la liste mot binaire >> motBinaire
+                motBinaire.push(parseInt(licenceGrayCode[i+decalage]));
+            }
+            //Conversion du mot binaire en taille de barre avec une recherche de première occurence
+            //Parcour des clé du dictionnaire CORRESP_GRAYCODE_BARRE
+            for(var posCourante in CORRESP_GRAYCODE_BARRE){
+                //Condition qui vérifie l'égalité entre le motBinaire et les valeurs du dictionnaire CORRESP_GRAYCODE_BARRE
+                if(JSON.stringify(motBinaire)===JSON.stringify(CORRESP_GRAYCODE_BARRE[posCourante])){
+                    //listeBarre, posCourante>> Affectation de la posCourante dans la liste des barres >> listeBarre 
+                    listeBarre.push(posCourante);
+                    break;
+                }
+            }
+        }    
+        //Retourne la liste des hauteurs de barres
+        return listeBarre
+    }
+
+    //Méthode : Aucune données >> genererCodeBarre >> Aucun résultat
+    genererCodeBarre(){
+        //Affectation dans une variable du résultat de la conversion de la licence en gray code
+        var licenceGrayCode = this.convertionGrayCode();
+        //Affectation dans une variable du résultat de la conversion de la licenceGrayCode en liste de barre
+        var listeBarre = this.conversionBarres(licenceGrayCode);
+        //Affichage du ST code
+        //Parcours complet du ST code avec traitement sytématique 
+        for(var i=0;i<listeBarre.length;i++){
+            //barreHTMLTemp >> Affectation de la hauteur pour les éléments grpahique correspondant aux barres >> barreHTMLTemp
+            var barreHTMLTemp = document.getElementById('vertical'+i);
+            //hateurBareTemp >> Affectation de la hauteur de barre dans une variable temporaire >> hauteurBarreTemp
+            var hauteurBarreTemp = 10*listeBarre[i];
+            //barreHTMLTemp, hauteurBarreTemp >> Affectation de la hauteur courante à l'élément graphique courant >> Aucun résultat
+            barreHTMLTemp.style.height = `${hauteurBarreTemp}px`
+            //Application de quelques rêgle CSS
+            barreHTMLTemp.style.borderLeft = "4px solid white"
+            barreHTMLTemp.style.display="inline-block"
+            barreHTMLTemp.style.marginLeft="5px"
+            barreHTMLTemp.style.borderRadius="5px"
+        }
+    }
+  }
+  
--- src/codeJavaScript/V2_DEV/decodage/main.js
+++ src/codeJavaScript/V2_DEV/decodage/main.js
@@ -6,121 +6,126 @@
 
 //MODIFICATION IMAGE
 // VARIABLES GLOBALES
-const TABLE_ENCODAGE_GRAY_CODE ={'a':'000000',
-                    'b':'000001',
-                    'c':'000011',
-                    'd':'000010',
-                    'e':'000110',
-                    'f':'000111',
-                    'g':'000101',
-                    'h':'000100',
-                    'i':'001100',
-                    'j':'001101',
-                    'k':'001111',
-                    'l':'001110',
-                    'm':'001010',
-                    'n':'001011',
-                    'o':'001001',
-                    'p':'001000',
-                    'q':'011000',
-                    'r':'011001',
-                    's':'011011',
-                    't':'011010',
-                    'u':'011110',
-                    'v':'011111',
-                    'w':'011101',
-                    'x':'011100',
-                    'y':'010100',
-                    'z':'010101',
-                    'A':'010111',
-                    'B':'010110',
-                    'C':'010010',
-                    'D':'010011',
-                    'E':'010001',
-                    'F':'010000',
-                    'G':'110000',
-                    'H':'110001',
-                    'I':'110011',
-                    'J':'110010',
-                    'K':'110110',
-                    'L':'110111',
-                    'M':'110101',
-                    'N':'110100',
-                    'O':'111100',
-                    'P':'111101',
-                    'Q':'111111',
-                    'R':'111110',
-                    'S':'111010',
-                    'T':'111011',
-                    'U':'111001',
-                    'V':'111000',
-                    'W':'101000',
-                    'X':'101001',
-                    'Y':'101011',
-                    'Z':'101010',
-                    0:'101110',
-                    1:'101111',
-                    2:'101101',
-                    3:'101100',
-                    4:'100100',
-                    5:'100101',
-                    6:'100111',
-                    7:'100110',
-                    8:'100010',
-                    9:'100011'};
+const TABLE_ENCODAGE_GRAY_CODE = {
+  'a': '000000',
+  'b': '000001',
+  'c': '000011',
+  'd': '000010',
+  'e': '000110',
+  'f': '000111',
+  'g': '000101',
+  'h': '000100',
+  'i': '001100',
+  'j': '001101',
+  'k': '001111',
+  'l': '001110',
+  'm': '001010',
+  'n': '001011',
+  'o': '001001',
+  'p': '001000',
+  'q': '011000',
+  'r': '011001',
+  's': '011011',
+  't': '011010',
+  'u': '011110',
+  'v': '011111',
+  'w': '011101',
+  'x': '011100',
+  'y': '010100',
+  'z': '010101',
+  'A': '010111',
+  'B': '010110',
+  'C': '010010',
+  'D': '010011',
+  'E': '010001',
+  'F': '010000',
+  'G': '110000',
+  'H': '110001',
+  'I': '110011',
+  'J': '110010',
+  'K': '110110',
+  'L': '110111',
+  'M': '110101',
+  'N': '110100',
+  'O': '111100',
+  'P': '111101',
+  'Q': '111111',
+  'R': '111110',
+  'S': '111010',
+  'T': '111011',
+  'U': '111001',
+  'V': '111000',
+  'W': '101000',
+  'X': '101001',
+  'Y': '101011',
+  'Z': '101010',
+  0: '101110',
+  1: '101111',
+  2: '101101',
+  3: '101100',
+  4: '100100',
+  5: '100101',
+  6: '100111',
+  7: '100110',
+  8: '100010',
+  9: '100011'
+};
 
-  const TABLE_DECODAGE=[[0.125,[0,0,0]],
-  [0.250,[0,0,1]],
-  [0.375,[0,1,1]],
-  [0.500,[1,1,1]],
-  [0.625,[1,1,0]],
-  [0.750,[1,0,0]],
-  [0.875,[1,0,1]],
-  [1,[0,1,0]]]
-  
-  
+const TABLE_DECODAGE = [[0.125, [0, 0, 0]],
+[0.250, [0, 0, 1]],
+[0.375, [0, 1, 1]],
+[0.500, [1, 1, 1]],
+[0.625, [1, 1, 0]],
+[0.750, [1, 0, 0]],
+[0.875, [1, 0, 1]],
+[1, [0, 1, 0]]]
+
+
 //Initiation des variables
 //Récupération des éléments de la page HTML par leur ID
-const parametres = { video:true, audio:false};
+const parametres = { video: true, audio: false };
 var video = document.getElementById("player");
 const photo = document.getElementById('canvas');
 const hiddenCanva = document.getElementById('hiddenCanvas');
 const context = photo.getContext('2d');
 const boutonPhoto = document.getElementById('photo');
-navigator.mediaDevices.getUserMedia(parametres).then(function(stream){player.srcObject = stream;});
+navigator.mediaDevices.getUserMedia(parametres).then(function (stream) { player.srcObject = stream; });
+
+
+
 // affichage de la video de la webcam
 
-function drawImg(){
+function drawImg() {
   var videoCanvas = document.getElementById("videoCanvas");
   var ctx = videoCanvas.getContext('2d');
   videoCanvas.width = video.videoWidth;
   videoCanvas.height = video.videoHeight;
-  
-  ctx.drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);  
+
+  ctx.drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);
   ctx.lineWidth = 2;
   let hauteur = 300
   let largeur = 150 // ratio 2:1
-  ctx.moveTo(180,130);
-  ctx.lineTo(190,130);
-  ctx.stroke();
-  
   ctx.moveTo(180, 130);
-  ctx.lineTo(180,140);
+  ctx.lineTo(190, 130);
+  ctx.stroke();
+
+  ctx.moveTo(180, 130);
+  ctx.lineTo(180, 140);
   ctx.stroke();
   //
   ctx.moveTo(480, 130);
   ctx.lineTo(470, 130);
   ctx.stroke();
-  
+
   ctx.moveTo(480, 130);
   ctx.lineTo(480, 140);
   ctx.stroke();
-  
+
   //
   ctx.moveTo(180, 280);
   ctx.lineTo(190, 280);
   ctx.stroke();
-  
+
   ctx.moveTo(180, 280);
   ctx.lineTo(180, 270);
   ctx.stroke();
@@ -128,28 +133,36 @@ function drawImg(){
   ctx.moveTo(480, 280);
   ctx.lineTo(470, 280);
   ctx.stroke();
-  
+
   ctx.moveTo(480, 280);
   ctx.lineTo(480, 270);
   ctx.stroke();
-  
-  setTimeout(drawImg , 100)
-  };
-  
+
+  setTimeout(drawImg, 100)
+};
+
 // 
-  video.onplay = function() {
-  setTimeout(drawImg , 300)};
+video.onplay = function () {
+  setTimeout(drawImg, 300)
+};
+
 
 // evenement photo
 boutonPhoto.addEventListener('click', () => {
-    // Canva a partir de la video
-    context.drawImage(player, 0, 0,photo.width, photo.height);
+  var erreur;
+  erreur = true;
 
-    // arrete la video    
-    //player.srcObject.getVideoTracks().forEach(track=> track.stop());
-    var hidden_ctx = hiddenCanvas.getContext('2d');
+  while (erreur == true) {
+    try {
+      console.log(erreur);
+      // Canva a partir de la video
+      context.drawImage(player, 0, 0, photo.width, photo.height);
 
-    hidden_ctx.drawImage(
+      // arrete la video    
+      //player.srcObject.getVideoTracks().forEach(track=> track.stop());
+      var hidden_ctx = hiddenCanvas.getContext('2d');
+
+      hidden_ctx.drawImage(
         photo,
         90, //x debut
         65, //y debut
@@ -159,16 +172,24 @@ boutonPhoto.addEventListener('click', () => {
         0,
         300,
         150
-    )
-    
+      )
 
-    let matriceImage = document.getElementById('hiddenCanvas');
-    let photoWebcam = new Photo(matriceImage);
-    var matriceContoursObjet = photoWebcam.recuperationContourObjets();
-    var listeRatio = photoWebcam.recuperationRatio(matriceContoursObjet);
-    var graycode = photoWebcam.conversionGrayCode(listeRatio);
-    var licence = photoWebcam.conversionLicence(graycode);
-    console.log(licence)
-    
 
+      let matriceImage = document.getElementById('hiddenCanvas');
+      let photoWebcam = new Photo(matriceImage);
+      var matriceContoursObjet = photoWebcam.recuperationContourObjets();
+      var listeRatio = photoWebcam.recuperationRatio(matriceContoursObjet);
+      var graycode = photoWebcam.conversionGrayCode(listeRatio);
+      var licence = photoWebcam.conversionLicence(graycode);
+      console.log(licence);
+
+      // On part du principe qu'il n'y a pas d'erreur a la fin du traitement
+      // Si jamais on trouve une erreur, on rentrera dans le catch
+      erreur = false;
+    }
+    catch (e) {
+      // s'il y a une erreur, on met erreur a vrai 
+      erreur = true;
+    }
+  }
 });
