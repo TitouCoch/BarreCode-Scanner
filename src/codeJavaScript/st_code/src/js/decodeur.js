7c125916e1682b888d32f170bc745866c932c4bb
Titouan Cocheril <titouan.cocheril25@gmail.com>
2022-12-13 18:23:58 +0100

    Scanneur fonctionnel, avec le rajout du code recupererRatio

--- src/codeJavaScript/st_code/src/js/decodeur.js
+++ src/codeJavaScript/st_code/src/js/decodeur.js
@@ -175,10 +175,10 @@ boutonPhoto.addEventListener('click', () => {
 
   let matriceImage = document.getElementById('hiddenCanvas');
   let desContoursObjets = new ContoursObjets(recuperationContourObjets(matriceImage));
-  var uneListeRatios = new ListeRatios(recuperationRatio(desContoursObjets));
+  var uneListeRatios = new ListeRatios(recuperationRatio(desContoursObjets.getContoursObjets));
   var licenceEnGrayCode = new GrayCode(conversionGrayCode(uneListeRatios));
   var uneLicence = new Licence(conversionLicence(licenceEnGrayCode));
-  testerLicence(uneLicence).then((n)=>{console.log(n)});
+  testerLicence(uneLicence).then((n) => { console.log(n) });
   /*    erreur = false;
     }
     catch (e) {
@@ -236,52 +236,60 @@ function recuperationContourObjets(uneMatriceImage) {
 
 //Méthode : matriceContoursObjets >> Récupération du ratio en fonction de la hauteur des barres obtenues sur la photo >> listeRatios
 function recuperationRatio(matriceContourObjet) {
-  //matriceContoursObjet >> RECUPERATION DES HAUTEURS ET POSITIONS DES OBJETS >> listeObjet
-  // listeObjets >> Initialisation >> listeObjets
-  var listeObjets = [];
-  //listeObjets >> Parcours complet de la matrice des contours objets avec traitement systématique >> listeObjets
-  for (var objetCourant = 0; objetCourant < matriceContourObjet.getContoursObjets().length; objetCourant++) {
-    //ymin, ymax, xpos >> Intialisation des variables avec le premier point de contours de lo'objet >> ymin, ymax, ypos
-    var ymin = matriceContourObjet.getContoursObjets()[objetCourant][0][1];
-    var ymax = matriceContourObjet.getContoursObjets()[objetCourant][0][1];
-    var xmin = matriceContourObjet.getContoursObjets()[objetCourant][0][0];
-    var xmax = matriceContourObjet.getContoursObjets()[objetCourant][0][0];
-    for (var point = 0; point < matriceContourObjet.getContoursObjets()[objetCourant].length; point++) {
-      if (matriceContourObjet.getContoursObjets()[objetCourant][point][1] > ymax) {
-        ymax = matriceContourObjet.getContoursObjets()[objetCourant][point][1];
+  function recupererListeObjets(matrice) {
+    //matriceContoursObjet >> RECUPERATION DES HAUTEURS ET POSITIONS DES OBJETS >> listeObjet
+    // listeObjets >> Initialisation >> listeObjets
+    var liste = [];
+    //listeObjets >> Parcours complet de la matrice des contours objets avec traitement systématique >> listeObjets
+    for (var objetCourant = 0; objetCourant < matrice.length; objetCourant++) {
+      //ymin, ymax, xpos >> Intialisation des variables avec le premier point de contours de lo'objet >> ymin, ymax, ypos
+      var ymin = matrice[objetCourant][0][1];
+      var ymax = matrice[objetCourant][0][1];
+      var xmin = matrice[objetCourant][0][0];
+      var xmax = matrice[objetCourant][0][0];
+      for (var point = 0; point < matrice[objetCourant].length; point++) {
+        if (matrice[objetCourant][point][1] > ymax) {
+          ymax = matrice[objetCourant][point][1];
+        }
+        if (matrice[objetCourant][point][1] < ymin) {
+          ymin = matrice[objetCourant][point][1];
+        }
+        if (matrice[objetCourant][point][0] > xmax) {
+          xmax = matrice[objetCourant][point][0];
+        }
+        if (matrice[objetCourant][point][0] < xmin) {
+          xmin = matrice[objetCourant][point][0];
+        }
       }
-      if (matriceContourObjet.getContoursObjets()[objetCourant][point][1] < ymin) {
-        ymin = matriceContourObjet.getContoursObjets()[objetCourant][point][1];
-      }
-      if (matriceContourObjet.getContoursObjets()[objetCourant][point][0] > xmax) {
-        xmax = matriceContourObjet.getContoursObjets()[objetCourant][point][0];
-      }
-      if (matriceContourObjet.getContoursObjets()[objetCourant][point][0] < xmin) {
-        xmin = matriceContourObjet.getContoursObjets()[objetCourant][point][0];
-      }
+      // calcule de la hauteur de la barre
+      var hauteur = ymax - ymin;
+      var largeur = xmax - xmin;
+      // ajout de la hauteur et de posx dans la listeObjets
+      liste.push([xmin, xmax, ymin, ymax, hauteur, largeur]);
     }
-    // calcule de la hauteur de la barre
-    var hauteur = ymax - ymin;
-    var largeur = xmax - xmin;
-    // ajout de la hauteur et de posx dans la listeObjets
-    listeObjets.push([xmin, xmax, ymin, ymax, hauteur, largeur]);
+    return liste;
   }
 
   //Fonction qui trouve les objets références dans l'image
-  function trouverReferences(listeObjets = []) {
+  function trouverReferences(listeObjets) {
     var listeReferences = [];
+    var marge = 3;
+    var XmaxLogo = -1;
     for (var i = 0; i < listeObjets.length; i++) {
-      var marge = 3;
       var hauteurTemp = listeObjets[i][4];
-      if (hauteurTemp - marge <= listeObjets[i][5] && listeObjets[i][5] <= hauteurTemp + marge) {
+      if (hauteurTemp - marge <= listeObjets[i][5] && listeObjets[i][5] <= hauteurTemp + marge && listeObjets[i][0] > XmaxLogo && hauteurTemp != 0) {
+        console.log(listeObjets[i])
         listeReferences.push(listeObjets[i]);
+        if (hauteurTemp >= 20) {
+          console.log(listeObjets[i])
+          XmaxLogo = listeObjets[i][1]
+        }
       }
     }
-    return listeReferences;
+    return listeReferences
   }
 
 
-  listeObjets.sort(fonctionTri);
   //creation de la fonction pour le tri
   function fonctionTri(a, b) {
     if (a[0] === b[0]) {
@@ -296,54 +304,24 @@ function recuperationRatio(matriceContourObjet) {
       }
     }
   }
-  var listeObjetsTrie = listeObjets;
 
-
-  //Appel de la fonction trouverReferences
-  var listeObjReference = trouverReferences(listeObjetsTrie);
-  var logo = listeObjReference[0];
-  var boule1 = listeObjReference[1];
-  var boule2 = listeObjReference[2];
-
-  function filtrerObjet(listeObjets = []) {
-    var marge = 10
-    var valeurXmin = boule1[0];
-    var valeurXmax = boule2[0];
-    var valeurYmin = logo[2];
-    var valeurYmax = logo[3];
-
-    for (var i = 1; i < listeObjets.length; i++) {
-      if (listeObjets[i][0] <= valeurXmin) {
-        //console.log(i,"L'objet est avant le logo sur l'axe x")
-        listeObjets.splice(i, 1);
-      }
-      else if (listeObjets[i][0] >= valeurXmax) {
-        //console.log(i,"L'objet est après le logo sur l'axe x")
-        listeObjets.splice(i, 1);
-      }
-      else if (listeObjets[i][2] < valeurYmin - 10) {
-        //console.log(i,"L'objet est avant le logo sur l'axe y")
-        listeObjets.splice(i, 1);
-      }
-      else if (listeObjets[i][3] > valeurYmax + 10) {
-        //console.log(i,"L'objet est après le logo sur l'axe y")
-        listeObjets.splice(i, 1);
-      }
-    }
-    //listeObjets.splice(0,1);
-    return listeObjets
-  }
-
-  var listeObjet2 = filtrerObjet(listeObjets)
   function distance(a, b) {
     return Math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
   }
+  function toDegree(a) {
+    return a * 180 / Math.PI;
+  }
+
+  function toRadian(a) {
+    return a / (180 / Math.PI);
+  }
   function calculerAngle(boule1, boule2) {
+
     // initialisation des points
     var pointA = [boule1[0] + boule1[4] / 2, boule1[2] + boule1[5] / 2];
     var pointB = [boule2[0] + boule2[4] / 2, boule2[2] + boule2[5] / 2];
     var pointVirtuel = [pointB[0], pointA[1]];
-    //
+    //afficher les droites
     contx.strokeStyle = '#f00';
     contx.beginPath();
     contx.moveTo(pointA[0], pointA[1]);
@@ -364,34 +342,102 @@ function recuperationRatio(matriceContourObjet) {
     var adjacent = distance(pointA, pointVirtuel);
     console.log("h:", hypothenuse, "adj:", adjacent);
     var angle = Math.acos(adjacent / hypothenuse);
-    if (Math.sin(angle) > 0) {
-      console.log("angle positif")
+    angle = toDegree(angle);
+    if (pointVirtuel[1] < pointB[1]) {
+      console.log("angle positif");
+      return angle;
     }
     else {
-      console.log("angle negatif")
+      console.log("angle negatif");
+      return -angle;
     }
-    return angle;
-  }
-  console.log(calculerAngle(boule1, boule2));
 
-  function rotationEnsemble(centre, matrice) {
-    function rotation(angle, point) {
-      return [point[0] * Math.cos(angle) + point[1] * Math.sin(angle), -(point[0] * Math.sin(angle)) + point[1] * Math.cos(angle)]
+  }
+
+  function rotationPoint(x, y, angle) {
+    var angleRadian = toRadian(angle)
+    return [x * Math.cos(angleRadian) + y * -1 * Math.sin(angleRadian), x * Math.sin(angleRadian) + y * Math.cos(angleRadian)]
+  }
+  function rotationEnsemble(centre, matrice, angle) {
+    var matriceApresRotation = [];
+    for (var objet = 0; objet < matrice.length; objet++) {
+      matriceApresRotation.push([])
+      for (var point = 0; point < matrice[objet].length; point++) {
+        // le y du point dans un repere cartesien de centre "centre"
+        var XRepereCentre = matrice[objet][point][0] - centre[0];
+        var YRepereCentre = matrice[objet][point][1] - centre[1];
+        // rotation des coordonées du point
+        var nouveauPoint = rotationPoint(XRepereCentre, YRepereCentre, angle);
+        // remettre les points dans le plan d'origine
+        var XRepereOrigine = nouveauPoint[0] + centre[0];
+        var YRepereOrigine = nouveauPoint[1] + centre[1];
+        // remplacer le point par le nouveau point apres rotation
+        matriceApresRotation[objet].push([XRepereOrigine, YRepereOrigine])
+      }
     }
+    return matriceApresRotation
   }
   // listeObjetTrie >> CALCUL DES RATIOS >> listeRatios
   // hauteurReference >> Affectation de  la hauteur du premier objet a la hauteur reference >> hauteurReference
-  var hauteurReference = listeObjetsTrie[0][1];
-  // hauteurReference, listeObjetsTrie >> Parcours et calcul de chaques ratios >> listeRatios
-  // listeRatios >> Initialisation variable >> listeRatios
-  var listeRatios = [];
-  for (var objetCourant = 1; objetCourant < listeObjetsTrie.length; objetCourant++) {
-    // listeRatios, hauteurReference, listeObjetTrie >> Division de la hauteur de l'objet courant par la hauteur référence et insertion dans la liste des ratios >> listeRatios
-    var ratio = (listeObjetsTrie[objetCourant][1] / hauteurReference);
-    listeRatios.push(ratio);
+  function calculerLesRatios(listeObjets) {
+    var hauteurReference = logo[4];
+    var listeRatios = [];
+    // hauteurReference, listeObjetsTrie >> Parcours et calcul de chaques ratios >> listeRatios
+    for (var objetCourant = 0; objetCourant < listeObjets.length; objetCourant++) {
+      {
+        // listeRatios, hauteurReference, listeObjetTrie >> Division de la hauteur de l'objet courant par la hauteur référence et insertion dans la liste des ratios >> listeRatios
+        var ratio = (listeObjets[objetCourant][4] / hauteurReference);
+        listeRatios.push(ratio);
+      }
+
+    }
+    return listeRatios;
   }
+  function filtreObjet(liste) {
+    var listeObjetFiltreApresRotation = [];
+    var limiteXmax = boule2[1];
+    var limiteXmin = boule1[0];
+    for (var i = 0; i < liste.length; i++) {
+      var objetCourant = liste[i];
+      if (objetCourant[0] >= limiteXmin && objetCourant[1] <= limiteXmax) {
+        listeObjetFiltreApresRotation.push(objetCourant)
+      }
+    }
+    return listeObjetFiltreApresRotation;
+  }
+  //for (var objet=0; objet<listeObjetsTrie)
+
+  var listeObjets = recupererListeObjets(matriceContourObjet);
+  var listeObjetsTrie = listeObjets.sort(fonctionTri);
+  console.log(listeObjetsTrie);
+  var listeReferences = trouverReferences(listeObjetsTrie);
+  var logo = listeReferences[0];
+  var boule1 = listeReferences[1];
+  var boule2 = listeReferences[2];
+  console.log("logo : ", logo);
+  console.log("boule1 : ", boule1);
+  console.log("boule2 : ", boule2);
+
+  var centreLogo = [logo[0] + (logo[1] - logo[2]) / 2, logo[2] + (logo[3] - logo[2]) / 2];
+  var angleRotation = calculerAngle(boule1, boule2);
+  var matriceObjetsApresRotation = rotationEnsemble(centreLogo, matriceContourObjet, angleRotation);
+  console.log("matrice Avant Filtrage", matriceObjetsApresRotation);
+
+  var listeObjetsApresRotation = recupererListeObjets(matriceObjetsApresRotation);
+  console.log("liste Objet Avant Filtrage", listeObjetsApresRotation);
+
+  var listeObjetsFiltreApresRotation = filtreObjet(listeObjetsApresRotation, logo, boule1, boule2);
+  console.log("liste Objet Apres Filtrage", listeObjetsFiltreApresRotation);
+
+  var listeObjetsFiltreEtTrieApresRotation = listeObjetsFiltreApresRotation.sort(fonctionTri);
+  console.log("liste Objet Apres trie", listeObjetsFiltreEtTrieApresRotation);
+
+  var listeRatios = calculerLesRatios(listeObjetsFiltreEtTrieApresRotation);
+
+  console.log(listeRatios)
   return listeRatios;
 };
+
 
 
 //Méthode : listeRatios >> Conversion barres en grey code >> licenceGrayCode
