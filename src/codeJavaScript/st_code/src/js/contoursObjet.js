1386241b7694a208978e424f9be5dd56732c5df0
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-12-14 16:43:14 +0100

    Ajout de la documentation pour les classes concernant l'encodage

--- src/codeJavaScript/st_code/src/js/contoursObjet.js
+++ src/codeJavaScript/st_code/src/js/contoursObjet.js
@@ -1,12 +1,38 @@
+/**
+ * Class représentant la matrice de contour
+ * 
+ * Explication :
+ * 
+ * @author Sport Track
+ */
 class ContoursObjets {
     //ATTRIBUTS et CONSTRUCTEUR
+    /**
+     * Constructeur de l'objet ContoursObjets.
+     *
+     * @param {} uneContoursObjets - Matrice de contour.
+     */
     constructor(uneContoursObjets) {
         this.ContoursObjets = uneContoursObjets;
         // ENCAPSULATION
     };
+
+    //Fonction qui modifie l'attribut uneContoursObjets
+    /**
+     * Modifie la matrice de contours.
+     * @param {} uneContoursObjets - Matrice de contour.
+     * 
+     */
     setContoursObjets(uneContoursObjets) {
         this.ContoursObjets = uneContoursObjets;
     };
+
+    //Fonction qui retourne l'attribut uneContoursObjets
+    /**
+     * Retourne la matrice de contours.
+     *
+     * @return {} Matrice de contour.
+     */
     getContoursObjets() {
         return this.ContoursObjets;
     };
