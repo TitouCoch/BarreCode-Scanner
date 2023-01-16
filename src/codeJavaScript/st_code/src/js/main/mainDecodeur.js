e55abfb6930dbed47d4d7eb76bb977e90b750675
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-01-16 15:02:00 +0100

    Rajout de détails

--- src/codeJavaScript/st_code/src/js/main/mainDecodeur.js
+++ src/codeJavaScript/st_code/src/js/main/mainDecodeur.js
@@ -1,7 +1,7 @@
-//Main decodeur
+//Main Decodeur 
 
 //Initiation des variables
-//Récupération des éléments de la page HTML par leur ID
+//Récupération des éléments de la page HTML par leur ID (context)
 const parametres = { video: true, audio: false };
 var video = document.getElementById("player");
 const photo = document.getElementById('canvas');
@@ -9,74 +9,57 @@ const hiddenCanvas = document.getElementById('hiddenCanvas');
 const context = photo.getContext('2d');
 const boutonPhoto = document.getElementById('photo');
 navigator.mediaDevices.getUserMedia(parametres).then(function (stream) { player.srcObject = stream; });
-//
 const output2 = document.getElementById('output2');
 var contx2 = output2.getContext('2d');
-
 const output3 = document.getElementById('output3');
 var contx3 = output3.getContext('2d');
-
 const output4 = document.getElementById('output4');
 var contx4 = output4.getContext('2d');
-
 const output5 = document.getElementById('output5');
 var contx5 = output5.getContext('2d');
 
-// affichage de la video de la webcam
-
+// Fonction d'affichage de la mire
 async function drawImg() {
   var videoCanvas = document.getElementById("videoCanvas");
   var ctx = videoCanvas.getContext('2d');
   videoCanvas.width = video.videoWidth;
   videoCanvas.height = video.videoHeight;
-
   ctx.drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);
   ctx.lineWidth = 2;
-  let hauteur = 300
-  let largeur = 150 // ratio 2:1
+  //Placement de la mire sur le retour de webcam
   ctx.moveTo(180, 130);
   ctx.lineTo(190, 130);
   ctx.stroke();
-
   ctx.moveTo(180, 130);
   ctx.lineTo(180, 140);
   ctx.stroke();
-  //
   ctx.moveTo(480, 130);
   ctx.lineTo(470, 130);
   ctx.stroke();
-
   ctx.moveTo(480, 130);
   ctx.lineTo(480, 140);
   ctx.stroke();
-
-  //
   ctx.moveTo(180, 280);
   ctx.lineTo(190, 280);
   ctx.stroke();
-
   ctx.moveTo(180, 280);
   ctx.lineTo(180, 270);
   ctx.stroke();
-  //
   ctx.moveTo(480, 280);
   ctx.lineTo(470, 280);
   ctx.stroke();
-
   ctx.moveTo(480, 280);
   ctx.lineTo(480, 270);
   ctx.stroke();
-
   setTimeout(drawImg, 100)
 };
 
-// 
-
+//Lancement de la caméra à l'écran
 video.onplay = async function () {
   setInterval(drawImg, 300);
 };
 
-// evenement photo
+//Fonction qui se lance lors du click sur un bouton et qui lance le scanneur
 boutonPhoto.addEventListener('click', async () => {
   var trouve = false;
   while (true) {
@@ -87,8 +70,7 @@ boutonPhoto.addEventListener('click', async () => {
       //player.srcObject.getVideoTracks().forEach(track=> track.stop());
       var hidden_ctx = await hiddenCanvas.getContext('2d');
 
-      hidden_ctx.drawImage(
-        photo,
+      hidden_ctx.drawImage(photo,
         90, //x debut
         65, //y debut
         150,// largeur (ratio 2:1)
@@ -99,6 +81,7 @@ boutonPhoto.addEventListener('click', async () => {
         150
       )
 
+      //Appelle des méthodes du scanneur
       let matriceImage = document.getElementById('hiddenCanvas');
       let ledecodeur = new Decodeur(matriceImage);
       var matriceContoursObjet = new ContoursObjets(ledecodeur.recuperationContourObjets());
@@ -118,13 +101,9 @@ boutonPhoto.addEventListener('click', async () => {
       console.log("LICENCE TROUVE ! FIN");
       break;
     }
-
   };
-  
   document.getElementById('boutonJoueur').style.display = 'block';
   document.getElementById('boutonJoueur').addEventListener('click', function() {
   window.location.assign('http://localhost:8888/SportTrack/src/codeJavaScript/st_code/src/php/joueur.php?licence='+numLicence.getNumLicence());
-
   });
-
 });
--- src/codeJavaScript/st_code/src/js/main/mainEncodeur.js
+++ src/codeJavaScript/st_code/src/js/main/mainEncodeur.js
@@ -1,8 +1,11 @@
 //MAIN ENCODEUR
 
+//Function main encodeur lancé lors du click sur un bouton
 function mainEncodeur(){
+//Récupération de la chaine saisi dans le input
 const name = document.getElementById('name').value;
 console.log(name);
+//Appelle des fonction d'encodeur
 var uneLicence = new Licence(name);
 var unEncodeur = new Encodeur(uneLicence.getNumLicence());
 var unGrayCode = new GrayCode(unEncodeur.convertionGrayCode(uneLicence.getNumLicence()));
